b83255988f2cb89ed14772053506137b58437d20
SeohyeonLee-MZC <lxx2241@mz.co.kr>
2026-07-29 17:37:00 +0900

    Add S3 bucket and ACL configuration

---
+++ infra/s3.tf
@@ -0,0 +1,9 @@
+# infra/s3.tf
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "wiz-demo-public-bucket"
+}
+
+resource "aws_s3_bucket_acl" "demo_acl" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  acl    = "public-read"
+}
